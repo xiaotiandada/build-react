183ba9dc489c7eac8e473f382179df17f2346c41
xiaotian <istianlei@qq.com>
2023-03-01 01:32:21 +0800

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,3 @@
+### Build react
+
+- https://pomb.us/build-your-own-react/
---
+++ index.js
@@ -0,0 +1,39 @@
+function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map(child => typeof child === 'object' ? child : createTextElement(child)),
+    },
+  }
+}
+
+function createTextElement(text) {
+  return {
+    type: "TEXT_ELEMENT",
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  }
+}
+
+function render(element, container) {
+  // TODO create dom nodes
+}
+
+const Didact = {
+  createElement,
+  render
+}
+
+const element = Didact.createElement(
+  "div",
+  { id: "foo" },
+  Didact.createElement("a", null, "bar"),
+  Didact.createElement("b")
+)
+
+const container = document.getElementById("root")
+
+Didact.render(element, container)
