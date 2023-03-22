81a5832fec60079802b8c394c35362215b307e89
xiaotian <istianlei@qq.com>
2023-03-22 16:44:13 +0800

    mini react

--- index.js
+++ index.js
@@ -25,31 +25,17 @@ function createTextElement(text) {
   }
 }
 
-function createDom(filber) {
+function createDom(fiber) {
   // 处理文本元素，如果元素类型是 TEXT_ELEMENT ，我们创建一个文本节点而不是常规节点。
   const dom =
-  filber.type === 'TEXT_ELEMENT'
+  fiber.type === 'TEXT_ELEMENT'
       ? document.createTextNode('')
-      : document.createElement(filber.type)
+      : document.createElement(fiber.type)
 
   // 将元素 props 分配给节点。
-  const isProperty = (key) => key !== 'children'
-  Object.keys(filber.props)
-    .filter(isProperty)
-    .forEach((name) => {
-      dom[name] = filber.props[name]
-    })
+  updateDom(dom, {}, fiber.props);
 
-    return dom
-}
-
-function commitRoot() {
-  // 当我们将更改提交到 DOM 时，我们还使用该数组中的 fiber。
-  deletions.forEach(commitWork)
-  commitWork(wipRoot.child)
-  // 在完成提交后保存对“我们提交给 DOM 的最后一个 fiber tree ”的引用。我们称它为 currentRoot 。
-  currentRoot = wipRoot
-  wipRoot = null
+  return dom
 }
 
 // 事件侦听器，以“on”前缀开头
@@ -98,16 +84,26 @@ function updateDom(dom, prevProps, nextProps) {
   // Add event listeners
   Object.keys(nextProps)
   .filter(isEvent)
-  .filter(isNew(prevProps, nextProps))
+  // .filter(isNew(prevProps, nextProps))
   .forEach(name => {
     const eventType = name
       .toLowerCase()
       .substring(2)
+    
     dom.addEventListener(
       eventType,
       nextProps[name]
     )
   })
+}
+
+function commitRoot() {
+  // 当我们将更改提交到 DOM 时，我们还使用该数组中的 fiber。
+  deletions.forEach(commitWork)
+  commitWork(wipRoot.child)
+  // 在完成提交后保存对“我们提交给 DOM 的最后一个 fiber tree ”的引用。我们称它为 currentRoot 。
+  currentRoot = wipRoot
+  wipRoot = null
 }
 
 // 找 child 然后找 sibling，最后找到 parent
@@ -237,12 +233,57 @@ function performUnitOfWork(fiber) {
   }
 }
 
+let wipFiber = null
+let hookIndex = null
+
 function updateFunctionComponent(fiber) {
+  // 设置正在进行的工作 fiber。
+  wipFiber = fiber
+  // 我们会跟踪当前的 hook index
+  hookIndex = 0
+  // 在 fiber 添加了一个 hooks 数组，以支持在同一个组件中多次调用 useState 。
+  wipFiber.hooks = []
   // get the children.
   // fiber.type 是函数，当我们运行它时，它返回 element
   const children = [fiber.type(fiber.props)]
   // 以同样的方式调用 reconcileChildren
   reconcileChildren(fiber, children)
+}
+
+function useState(initial) {
+  // 当函数组件调用 useState 时，我们检查是否有 old hook。我们使用 hook index 检查 fiber 的 alternate 。
+  const oldHook = wipFiber.alternate && wipFiber.alternate.hooks && wipFiber.alternate.hooks[hookIndex]
+  // 如果我们有一个 old hook，我们将状态从 old hook 复制到 new hook，如果我们没有，我们初始化状态。
+  const hook = {
+    state: oldHook ? oldHook.state : initial,
+    queue: []
+  }
+
+  // 我们在下次渲染组件时这样做，我们从 old hook 队列中获取所有动作
+  const actions = oldHook ? oldHook.queue : []
+  // 然后将它们一个一个地应用到 new hook 状态，所以当我们返回状态时它被更新了。
+  actions.forEach(action => {
+    hook.state = action(hook.state)
+  })
+
+  const setState = action => {
+    // 我们将该操作推送到我们添加到 hook 的队列中。
+    hook.queue.push(action)
+    // 然后我们做一些类似于我们在 render 函数中所做的事情，设置一个新的正在进行的工作根作为下一个工作单元，这样工作循环就可以开始一个新的渲染阶段。
+    wipRoot = {
+      dom: currentRoot.dom,
+      props: currentRoot.props,
+      alternate: currentRoot
+    }
+    nextUnitOfWork = wipRoot
+    deletions = []
+  }
+  
+  // 然后我们将 new hook 添加到 fiber 中，将 hook index 加一，
+  wipFiber.hooks.push(hook)
+  hookIndex++
+  // 然后返回状态。useState 还应该返回一个函数来更新状态，因此我们定义了一个接收动作的 setState 函数
+  return [hook.state, setState]
 }
 
 function updateHostComponent(fiber) {
@@ -324,7 +365,19 @@ function reconcileChildren(wipFiber, elements) {
 const Didact = {
   createElement,
   render,
+  useState
 }
+
+/** @jsx Didact.createElement */
+function Counter() {
+  const [state, setState] = Didact.useState(1);
+  return Didact.createElement("div", null, Didact.createElement("h1", {
+    onClick: () => setState(c => c + 1)
+  }, "Count: ", state), Didact.createElement("button", {
+    onClick: () => console.log('click event')
+  }, "event"));
+}
+const elementState = Didact.createElement(Counter, null);
 
 /** @jsx Didact.createElement */
 function App(props) {
@@ -355,7 +408,8 @@ const element = Didact.createElement(
     },
     'from Didact'
   ),
-  elementApp
+  elementApp,
+  elementState
 )
 
 const container = document.getElementById('root')
