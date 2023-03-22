8c3596ecc16518f2a42970823aaa44ae39ecefe5
xiaotian <istianlei@qq.com>
2023-03-22 19:19:00 +0800

    feat: ReactElement

--- Readme.md
+++ Readme.md
@@ -1,4 +1,5 @@
 ### Build react
 
 - https://github.com/facebook/react/
+- https://github1s.com/facebook/react/blob/HEAD/packages/react/src/React.js
 - https://pomb.us/build-your-own-react/
--- src/ReactElement.ts
+++ src/ReactElement.ts
@@ -1,5 +1,12 @@
 import { FunctionComponent, Props } from '../index.d'
 
+const RESERVED_PROPS = {
+  key: true,
+  ref: true,
+  __self: true,
+  __source: true,
+}
+
 // 使用 textNode 而不是设置 innerText 将允许我们稍后以相同的方式处理所有元素
 // 还要注意我们如何设置 nodeValue 就像我们对 h1 标题所做的那样，它几乎就像字符串有 props: {nodeValue: "hello"} 一样。
 // children 数组还可以包含原始值，如字符串或数字。所以我们将所有不是对象的东西都包装在它自己的元素中，并为它们创建一个特殊类型： TEXT_ELEMENT 。
@@ -20,13 +27,28 @@ export function createElement(
   config: Props | null,
   ...children: any[]
 ) {
+  let propName: any
+
+  // Reserved names are extracted
+  const props: Props = {}
+
+  if (config != null) {
+    for (propName in config) {
+      if (
+        config.hasOwnProperty(propName) &&
+        !RESERVED_PROPS.hasOwnProperty(propName)
+      ) {
+        props[propName] = config[propName]
+      }
+    }
+  }
+
+  props.children = children.map((child) =>
+    typeof child === 'object' ? child : createTextElement(child)
+  )
+
   return {
     type,
-    props: {
-      ...config,
-      children: children.map((child) =>
-        typeof child === 'object' ? child : createTextElement(child)
-      ),
-    },
+    props,
   }
 }
