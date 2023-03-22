cd0723965a01d88bb28dbb99d405ea44b096274a
xiaotian <istianlei@qq.com>
2023-03-22 15:22:13 +0800

    Update index.js

--- index.js
+++ index.js
@@ -44,17 +44,21 @@ function createDom(filber) {
 }
 
 function commitRoot() {
+  // 当我们将更改提交到 DOM 时，我们还使用该数组中的 fiber。
   deletions.forEach(commitWork)
   commitWork(wipRoot.child)
+  // 在完成提交后保存对“我们提交给 DOM 的最后一个 fiber tree ”的引用。我们称它为 currentRoot 。
   currentRoot = wipRoot
   wipRoot = null
 }
 
+// 事件侦听器，以“on”前缀开头
 const isEvent = key => key.startsWith("on")
 const isProperty = key => key !== "children" && !isEvent(key)
 const isNew = (prev, next) => key => prev[key] !== next[key]
 const isGone = (prev, next) => key => !(key in next)
 
+// 我们将旧 fiber 的 props 与新 fiber 的 props 进行比较，移除消失的 props，并设置新的或更改的 props。
 function updateDom(dom, prevProps, nextProps) {
   //Remove old or changed event listeners
   Object.keys(prevProps)
@@ -91,7 +95,7 @@ function updateDom(dom, prevProps, nextProps) {
       dom[name] = nextProps[name]
     })
 
-   // Add event listeners
+  // Add event listeners
   Object.keys(nextProps)
   .filter(isEvent)
   .filter(isNew(prevProps, nextProps))
@@ -107,6 +111,7 @@ function updateDom(dom, prevProps, nextProps) {
 }
 
 // 找 child 然后找 sibling，最后找到 parent
+// 递归地将所有节点附加到 dom。
 function commitWork(fiber) {
   console.log('commitWork', fiber);
   if (!fiber) {
@@ -114,6 +119,7 @@ function commitWork(fiber) {
   }
 
   const domParent = fiber.parent.dom
+  // 如果 fiber 有一个 PLACEMENT effect 标签，我们和以前一样，将 DOM 节点附加到父 fiber 的节点。
   if (
     fiber.effectTag === "PLACEMENT" &&
     fiber.dom != null
@@ -123,12 +129,14 @@ function commitWork(fiber) {
     fiber.effectTag === "UPDATE" &&
     fiber.dom != null
   ) {
+    // 如果它是一个 UPDATE ，我们需要用改变的属性更新现有的 DOM 节点。
     updateDom(
       fiber.dom,
       fiber.alternate.props,
       fiber.props
     )
   } else if (fiber.effectTag === "DELETION") {
+    // 如果是 DELETION ，我们做相反的事情，删除孩子。
     domParent.removeChild(fiber.dom)
   }
 
@@ -144,15 +152,21 @@ function render(element, container) {
     props: {
       children: [element]
     },
+    // 将 alternate 属性添加到每个 fiber 。此属性是指向 old fiber 的链接，old fiber 是我们在前一个提交阶段提交给 DOM 的 fiber。
     alternate: currentRoot
   }
   deletions = []
   nextUnitOfWork = wipRoot
 }
 
+// 下一个工作单元
 let nextUnitOfWork = null
+// 当前 Root
 let currentRoot = null
+// 将跟踪 fiber tree。我们称它为正在进行的工作 root 或 wipRoot 。
 let wipRoot = null
+// 但是当我们将纤程树提交给 DOM 时，我们是从正在进行的工作根目录中执行的，它没有 old fiber。
+// 所以我们需要一个数组来跟踪我们要删除的节点。
 let deletions = null
 
 // 所以我们要把工作分解成小的单元，当我们完成每个单元后，如果还有其他需要做的事情，我们会让浏览器中断渲染。
@@ -169,6 +183,7 @@ function workLoop(deadline) {
     shouldYield = deadline.timeRemaining() < 1
   }
 
+  // 一旦我们完成所有工作（我们知道它是因为没有下一个工作单元），我们将整个 fiber 树提交给 DOM。
   if (!nextUnitOfWork && wipRoot) {
     commitRoot()
   }
@@ -209,11 +224,16 @@ function performUnitOfWork(fiber) {
   }
 }
 
+// 创建 new fiber
 function reconcileChildren(wipFiber, elements) {
   let index = 0
+  // old fiber
   let oldFiber = wipFiber.alternate && wipFiber.alternate.child
   let prevSibling = null
 
+  // 同时遍历 old fiber ( wipFiber.alternate ) 的子节点和我们想要协调的元素数组。
+  // 如果我们忽略同时遍历一个数组和一个链表所需的所有样板，那么我们只剩下 while 中最重要的内容： oldFiber 和 element 。
+  // element 是我们要渲染到 DOM 的东西， oldFiber 是我们上次渲染的东西。
   // 为每个孩子创建一个 new fiber。
   while (index < elements.length || oldFiber != null) {
     const element = elements[index]
@@ -221,7 +241,10 @@ function reconcileChildren(wipFiber, elements) {
 
     const sameType = oldFiber && element && element.type == oldFiber.type
 
+    // 如果旧的 fiber 和新的元素有相同的类型，我们可以保留 DOM 节点并用新的 props 更新它
     if (sameType) {
+      // 当旧的 fiber 和元素具有相同的类型时，我们创建一个新的 fiber，保留旧 fiber 的 DOM 节点和元素的 props
+      // 我们还向纤程添加了一个新属性： effectTag 。
       newFiber = {
         type: oldFiber.type,
         props: element.props,
@@ -231,8 +254,9 @@ function reconcileChildren(wipFiber, elements) {
         effectTag: "UPDATE"
       }
     }
-
+    // 如果类型不同并且有一个新元素，则意味着我们需要创建一个新的 DOM 节点
     if (element && !sameType) {
+      // 然后，对于元素需要新 DOM 节点的情况，我们使用 PLACEMENT effect 标签标记 new fiber。
       newFiber = {
         type: element.type,
         props: element.props,
@@ -243,7 +267,9 @@ function reconcileChildren(wipFiber, elements) {
       }
     }
 
+    // 如果类型不同并且有旧光纤，我们需要删除旧节点
     if (oldFiber && !sameType) {
+      // 对于我们需要删除节点的情况，我们没有新的 fiber，所以我们将 effect 标签添加到旧的 fiber。
       oldFiber.effectTag = "DELETION"
       deletions.push(oldFiber)
     }
